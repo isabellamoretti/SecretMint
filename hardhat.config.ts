ae5e8acf62a7c4313f8a9e6a0e1d0cd9b34539f1
isabellamoretti <pczagetu556@outlook.com>
2025-12-24 13:28:43 +0800

    Wire up Hardhat config for SecretMint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,16 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
 
 import "./tasks/accounts";
-import "./tasks/FHECounter";
+import "./tasks/SecretMint";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "";
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -34,26 +34,15 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
       chainId: 31337,
     },
-    anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
+    localhost: {
+      accounts: "remote",
       chainId: 31337,
-      url: "http://localhost:8545",
+      url: "http://127.0.0.1:8545",
     },
     sepolia: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
       chainId: 11155111,
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
     },
